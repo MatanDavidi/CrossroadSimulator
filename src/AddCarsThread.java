edfa58585b7a0c1f1218de08baa6a06ecaeed801
Matan Davidi <davidi.matan@gmail.com>
2019-01-06 16:03:02 +0100

    Set maximum number of cars at the crossroad to 15 to make the program lighter

--- src/AddCarsThread.java
+++ src/AddCarsThread.java
@@ -48,7 +48,7 @@ public class AddCarsThread extends Thread {
     @Override
     public void run() {
 
-        while (true) {
+        while (crossroad.getDownCars().size() + crossroad.getLeftCars().size() + crossroad.getRightCars().size() + crossroad.getUpCars().size() <= 15) {
 
             try {
 
