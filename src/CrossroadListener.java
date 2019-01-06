5e2b629dc36f2f78428e6b78a6e77275c39c9e41
Matan Davidi <davidi.matan@gmail.com>
2019-01-06 22:35:04 +0100

    Added javadoc comments to interface CrossroadListener

--- src/CrossroadListener.java
+++ src/CrossroadListener.java
@@ -16,26 +16,41 @@
  */
 
 /**
+ * The listener interface for receiving crossroad events (car added, car passed,
+ * car passing) on a component. (To track changes made on the crossroad.)
+ * The class that is interested in processing a mouse event implements this
+ * interface.
+ * The listener object created from that class is then registered with a
+ * crossroad using the crossroad's addCrossroadListener
+ * A crossroad event is generated when a car is added, passing or after it has
+ * passed.
+ * When a crossroad event occurs, the relevant method in the listener object is
+ * invoked, and the Car that caused the invocation is passed to it.
  *
  * @author Matan Davidi
+ * @version ‎31-dec-2018
  */
 public interface CrossroadListener {
 
     /**
+     * Invoked whenever a car is added to the crossroad
      *
-     * @param source
+     * @param source the car that was added to the crossroad
      */
     public void carAdded(Car source);
 
     /**
+     * Invoked whenever a car has passed the crossroad
      *
-     * @param source
+     * @param source the car that passed the crossroad
      */
     public void carPassed(Car source);
 
     /**
+     * Invoked whenever a car is in the middle of passing through the
+     * crossroad
      *
-     * @param source
+     * @param source the car that is passing the crossroad
      */
     public void carPassing(Car source);
 
